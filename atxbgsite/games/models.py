63a0e2b89f0e51fe9a1d672329400954de03c6eb
dcantilo <danielcantilo@gmail.com>
2021-12-22 13:15:13 -0600

    Ok got all the way back to where I was after starting from scratch, but at least now I have a more revealing error code to work with: AttributeError: 'ManyToOneRel' object has no attribute 'value_from_object'
    I think the DB got too clobbered as I was frequently redefining, dropping, adding models trying to get the intended Game-Genre M2M schema to work...

--- atxbgsite/games/models.py
+++ atxbgsite/games/models.py
@@ -1,5 +1,5 @@
 import datetime
-
+from django.utils import timezone as django_utils_timezone
 from django.db import models
 from modelcluster.fields import ParentalKey
 
@@ -11,17 +11,17 @@ from wagtail.snippets.edit_handlers import SnippetChooserPanel
 from wagtail.search import index
 
 
-# # Create your models here.
-# class GenresOrderable(Orderable):
-#     page = ParentalKey('games.GamePage', related_name='genres')
-#     genre = models.ForeignKey(
-#         'games.Genre',
-#         on_delete=models.CASCADE
-#     )
-#
-#     panels = [
-#         SnippetChooserPanel('genre')
-#     ]
+# Create your models here.
+class GenresOrderable(Orderable):
+    page = ParentalKey('games.GamePage', related_name='genres')
+    genre = models.ForeignKey(
+        'games.Genre',
+        on_delete=models.CASCADE
+    )
+
+    panels = [
+        SnippetChooserPanel('genre')
+    ]
 
 
 @register_snippet
@@ -45,55 +45,55 @@ class Genre(models.Model):
         verbose_name_plural = 'Genres'
 
 
-# class GamesIndexPage(Page):
-#     intro = RichTextField(blank=True)
-#
-#     content_panels = Page.content_panels + [
-#         FieldPanel('intro', classname="full")
-#     ]
-#
-#     subpage_types = ('games.GamePage',)
-#
-#
-# class GamePage(Page):
-#     # title = models.CharField(max_length=30)
-#     # genre = models.ForeignKey(
-#     #     'games.Genre',
-#     #     null=True,
-#     #     blank=True,
-#     #     on_delete=models.SET_NULL,
-#     #     related_name='+'
-#     # )
-#     genre = models.ManyToManyField(GenresOrderable, related_name='genres')
-#     description = models.CharField(max_length=500)
-#     date_added = models.DateField(default=datetime.date.today())
-#     min_players = models.IntegerField(default=1)
-#     max_players = models.IntegerField()
-#     duration = models.TimeField(null=True)
-#     complexity = models.FloatField(null=True)
-#     competitiveness = models.FloatField(null=True)
-#
-#     # TODO: -Teams, -Roles, -Reviews <- add attributes to model
-#     # TODO: add edit fields for rest of attributes since these are mostly req fields
-#
-#     search_fields = Page.search_fields + [
-#         # index.SearchField('title'),
-#         # index.SearchField('genres'),
-#         index.SearchField('description'),
-#     ]
-#
-#     content_panels = Page.content_panels + [
-#         # SnippetChooserPanel('genre'),
-#         # InlinePanel('genres', label='Genre'),
-#         FieldPanel('date_added'),
-#         FieldPanel('description', classname="full"),
-#         FieldPanel('min_players'),
-#         FieldPanel('max_players'),
-#         FieldPanel('duration'),
-#         FieldPanel('complexity'),
-#         FieldPanel('competitiveness'),
-#     ]
-#
+class GamesIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname="full")
+    ]
+
+    subpage_types = ('games.GamePage',)
+
+
+class GamePage(Page):
+    # title = models.CharField(max_length=30)
+    # genre = models.ForeignKey(
+    #     'games.Genre',
+    #     null=True,
+    #     blank=True,
+    #     on_delete=models.SET_NULL,
+    #     related_name='+'
+    # )
+    # genre = models.ManyToManyField(GenresOrderable, related_name='genres')
+    description = models.CharField(max_length=500)
+    date_added = models.DateField(default=django_utils_timezone.now)
+    min_players = models.IntegerField(default=1)
+    max_players = models.IntegerField()
+    duration = models.TimeField(null=True)
+    complexity = models.FloatField(null=True)
+    competitiveness = models.FloatField(null=True)
+
+    # TODO: -Teams, -Roles, -Reviews <- add attributes to model
+    # TODO: add edit fields for rest of attributes since these are mostly req fields
+
+    search_fields = Page.search_fields + [
+        index.SearchField('title'),
+        index.SearchField('genres'),
+        index.SearchField('description'),
+    ]
+
+    content_panels = Page.content_panels + [
+        # SnippetChooserPanel('genre'),
+        InlinePanel('genres', label='Genre'),
+        FieldPanel('date_added'),
+        FieldPanel('description', classname="full"),
+        FieldPanel('min_players'),
+        FieldPanel('max_players'),
+        FieldPanel('duration'),
+        FieldPanel('complexity'),
+        FieldPanel('competitiveness'),
+    ]
+
 
 # class GenresIndexPage(Page):
 #     intro = RichTextField(blank=True)
